ddd31102d467727e7a0f89d3fc50138affb90146
cnrdh <cnrdh@users.noreply.github.com>
2015-10-19 19:05:03 +0200

    Autocomplete

--- src/routes.js
+++ src/routes.js
@@ -14,7 +14,7 @@ var router = function ($routeProvider, $locationProvider) {
   template: '<npdc:formula></npdc:formula>',
   controller: 'PublicationEditController'
   }).when('/', {
-  template: '<npdc:search></npdc:search>',
+  template: '<npdc:autocomplete></npdc:autocomplete><npdc:search></npdc:search>',
   controller: 'PublicationSearchController'
   });
 };
--- src/search/PublicationSearchController.js
+++ src/search/PublicationSearchController.js
@@ -7,11 +7,16 @@ var PublicationSearchController = function ($scope, $location, $controller, Publ
   $controller('NpolarBaseController', { $scope: $scope });
   $scope.resource = Publication;
   
-  let defaults = { limit: 50, sort: "-published_sort", fields: 'title,id', facets: "topics,publication_type,state,draft", score: true };
+  let defaults = { limit: 50, sort: "-published_sort,-updated", fields: 'title,id', facets: "publication_type,state,topics", score: true };
   let invariants = $scope.security.isAuthenticated() ? {} : { "not-draft": "yes" } ;  
   let query = Object.assign(defaults, $location.search(), invariants);
   
   $scope.search(query);
+  
+  NpdcAutocompleteConfig.selectedDefault = ['publication'];
+  NpdcAutocompleteConfig.placeholder = 'Search publications';
+  NpdcAutocompleteConfig.query = defaults;
+  
 
 };
 
