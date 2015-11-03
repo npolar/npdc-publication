b8242524d64ab1fdaf16343b6505e5cabbf8aee1
RDuX <ruben.dens@gmail.com>
2015-11-03 15:11:07 +0100

    configure search results

--- src/search/PublicationSearchController.js
+++ src/search/PublicationSearchController.js
@@ -2,21 +2,21 @@
 /**
  * @ngInject
  */
-var PublicationSearchController = function ($scope, $location, $controller, Publication, $mdToast) {
+var PublicationSearchController = function ($scope, $location, $controller, Publication, $mdToast, npdcAppConfig) {
 
   $controller('NpolarBaseController', { $scope: $scope });
   $scope.resource = Publication;
-  
-  let defaults = { limit: 50, sort: "-published_sort,-updated", fields: 'title,id', facets: "publication_type,state,topics", score: true };
-  let invariants = $scope.security.isAuthenticated() ? {} : { "not-draft": "yes" } ;  
+
+  let defaults = { limit: 50, sort: "-published_sort,-updated", fields: 'title,id,updated,publication_type', facets: "publication_type,state,topics", score: true };
+  let invariants = $scope.security.isAuthenticated() ? {} : { "not-draft": "yes" } ;
   let query = Object.assign(defaults, $location.search(), invariants);
-  
+
   $scope.search(query);
-  
-  NpdcAutocompleteConfig.selectedDefault = ['publication'];
-  NpdcAutocompleteConfig.placeholder = 'Search publications';
-  NpdcAutocompleteConfig.query = defaults;
-  
+
+  npdcAppConfig.cardTitle = 'Publications';
+  npdcAppConfig.search.results = {
+    subtitle: 'publication_type'
+  };
 
 };
 
