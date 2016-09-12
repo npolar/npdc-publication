2173a62d13d881e34fccf1823c73f5c9ecb6daf8
srldl <siri.uldal@gmail.com>
2016-09-12 09:28:51 +0200

    updated links

--- src/edit/Publication.js
+++ src/edit/Publication.js
@@ -2,13 +2,13 @@
 
 function Publication(PublicationResource, NpolarApiSecurity) {
   'ngInject';
-  
+
   const schema = 'http://api.npolar.no/schema/publication-1';
-  
+
   return Object.assign(PublicationResource, {
-    
+
     schema,
-    
+
     create: function() {
 
       let user = NpolarApiSecurity.getUser();
@@ -16,7 +16,7 @@ function Publication(PublicationResource, NpolarApiSecurity) {
       let email = user.email;
       let [first_name,last_name] = user.name.split(' ');
       let organisation = user.email.split('@')[1];
-      
+
       user = { id, roles: ['author'], first_name, last_name, email, organisation};
       let collection = 'publication';
       let publication_type = 'other';
@@ -24,17 +24,17 @@ function Publication(PublicationResource, NpolarApiSecurity) {
       let title = `Publication created by ${user.email} ${new Date().toISOString()}`;
       let people = [user];
       let locations = [{ country: 'NO'}];
-      
-    
+
+
       //let id = PublicationResource.randomUUID();
       let p = { title, collection, schema, people, publication_type, topics, locations,
         state:'published', draft:'no'
       };
       console.debug(p);
       return p;
-      
+
     },
-    
+
     hashiObject: function(file) {
       return {
         url: file.uri,
@@ -45,7 +45,7 @@ function Publication(PublicationResource, NpolarApiSecurity) {
         content_type: file.type
       };
     },
-  
+
     fileObject: function(hashi) {
       return {
         uri: hashi.url,
@@ -55,8 +55,8 @@ function Publication(PublicationResource, NpolarApiSecurity) {
         type: hashi.content_type
       };
     }
-      
+
   });
-  
+
 }
 module.exports = Publication;
